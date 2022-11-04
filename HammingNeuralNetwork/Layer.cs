bd5f199f123912c2d41f3cd670a0b051b287ddcd
RuslanShugaipov <nabon1812@mail.ru>
2022-11-04 18:53:34 +0300

    Удалил ненужные комментарии.

--- HammingNeuralNetwork/Layer.cs
+++ HammingNeuralNetwork/Layer.cs
@@ -1,30 +1,4 @@
-﻿//using System.Collections.Generic;
-
-//namespace HammingNeuralNetwork
-//{
-//    public class Layer
-//    {
-//        public List<Neuron> Neurons { get; }
-//        public int Count => Neurons?.Count ?? 0;
-
-//        public Layer(List<Neuron> neurons, NeuronType type = NeuronType.S)
-//        {
-//            Neurons = neurons;
-//        }
-
-//        public List<double> GetSignals()
-//        {
-//            var result = new List<double>();
-//            foreach(var neuron in Neurons)
-//            {
-//                result.Add(neuron.Output);
-//            }
-//            return result;
-//        }
-//    }
-//}
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace HammingNeuralNetwork
 {
